a03919c0a0e75f5be831f253971e403e03d74d58
Tien Pham <tmpham1@uci.edu>
2018-08-09 12:02:44 -0700

    added dummy data

---
+++ src/data/todo.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        title:'Watch Naruto',
+        details: "Watch the final Arc and finish up the series to watch Boruto",
+        _id: '001',
+
+    },
+    {
+        title:'Watch Naruto Movie: Bonds',
+        details: "Watch the movies",
+        _id: '002',
+
+    },
+    {
+        title:'Watch Naruto: the Stone of Gelel ',
+        details: "Watch more of the movies",
+        _id: '003',
+
+    },
+    {
+        title:'Watch Naruto: The Last Tower',
+        details: "Watch movies to increase knowledge",
+        _id: '004',
+
+    },
+    {
+        title:'Watch Naruto: Road To Ninja',
+        details: "Watch Movies to keep up",
+        _id: '005',
+
+    },
+];
